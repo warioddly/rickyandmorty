4c67d69bd2b5f2ddc00d32eedfafe1c3b1f07ae4
warioddly <53710487+warioddly@users.noreply.github.com>
2024-12-02 18:02:27 +0600

    init

---
+++ RickyAndMorty/RickyAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyAndMortyApp.swift
+//  RickyAndMorty
+//
+//  Created by GØDØFIMØ on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RickyAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
